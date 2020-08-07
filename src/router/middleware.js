32dac0532b299a95ad95d330bd82728cb36072d2
Merouaneboucenna <imarwanee@gmail.com>
2020-08-07 22:49:49 +0100

    create central method for before&after

--- src/router/middleware.js
+++ src/router/middleware.js
@@ -5,36 +5,31 @@ export default class middleware {
 
     constructor(router) {
 
-        let _ro_ms
         router.beforeEach((to, from, next) => {
-            _ro_ms = route.get()
-                .filter(e => e.routes.findIndex(e => e.path === to.path) > -1)
-                .flatMap(e => e.middlewares)
-
-            _ro_ms.forEach(_ro_m => {
-                if (Object.keys(middlewares).indexOf(_ro_m) > -1)
-                    if (middlewares[_ro_m].before)
-                        middlewares[_ro_m].before({ to, from })
-                            .then(res => res ? next(`/${res}`) : next())
-                            .catch(code => next(`/${code}`))
-            })
+            this.standrad({ to, from, next }, "before")
         })
 
         router.afterEach((to, from) => {
-            if (!_ro_ms)
-                _ro_ms = route.get()
-                    .filter(e => e.routes.findIndex(e => e.path === to.path) > -1)
-                    .flatMap(e => e.middlewares)
-            _ro_ms.forEach(_ro_m => {
-                if (Object.keys(middlewares).indexOf(_ro_m) > -1)
-                    if (middlewares[_ro_m].after)
-                        middlewares[_ro_m].after({ to, from })
-                            .then(res => res ? next(`/${res}`) : next())
-                            .catch(code => next(`/${code}`))
-            })
+            this.standrad({ to, from }, "after")
+        })
+    }
+
+
+    standrad({ to, from, next }, doctrine) {
+        let _ro_ms = route.get()
+            .filter(e => e.routes.findIndex(e => e.path === to.path) > -1)
+            .flatMap(e => e.middlewares)
+
+        _ro_ms.forEach(_ro_m => {
+            if (Object.keys(middlewares).indexOf(_ro_m) > -1 && middlewares[_ro_m][doctrine])
+                if (doctrine == "before")
+                    middlewares[_ro_m][doctrine]({ to, from })
+                        .then(res => res ? next(`/${res}`) : next())
+                        .catch(code => next(`/${code}`))
+                else
+                    middlewares[_ro_m][doctrine]({ to, from })
 
         })
-
     }
 
 
